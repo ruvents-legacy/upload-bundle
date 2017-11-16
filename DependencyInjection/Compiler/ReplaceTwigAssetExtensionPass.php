cdffd0599ba393ab38f41349c6fa81e61bccbac5
Valentin <udaltsov.valentin@gmail.com>
2017-11-17 00:55:17 +0300

    Overrode {{ asset() }} to add support for AbstractUpload objects

---
+++ DependencyInjection/Compiler/ReplaceTwigAssetExtensionPass.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Ruvents\UploadBundle\DependencyInjection\Compiler;
+
+use Ruvents\UploadBundle\Twig\Extension\AssetExtension;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class ReplaceTwigAssetExtensionPass implements CompilerPassInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->has($name = 'twig.extension.assets')) {
+            return;
+        }
+
+        $container
+            ->findDefinition($name)
+            ->setClass(AssetExtension::class);
+    }
+}
--- RuventsUploadBundle.php
+++ RuventsUploadBundle.php
@@ -3,8 +3,17 @@ declare(strict_types=1);
 
 namespace Ruvents\UploadBundle;
 
+use Ruvents\UploadBundle\DependencyInjection\Compiler\ReplaceTwigAssetExtensionPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class RuventsUploadBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new ReplaceTwigAssetExtensionPass());
+    }
 }
